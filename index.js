71eedb9777bc28e7cfb2519fd701e1c998d37a12
Dreams0809 <expensivepain9@gmail.com>
2024-02-04 23:15:13 -0500

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-document.querySelector('h1').onclick = changeColor
+// const x = 'EE352E'
+// const y = '00933C'
+// const z=  'B933AD'
 
-function changeColor(){
-    document.getElementsByClassName('color').style.color = 'red'
-   
+// function changeColor(){
+     
+//     const randomColor = {x,y,x}
+// }
 
-}
+
+// document.querySelector('h1').addEventListener('click',changeColor)
+
+// function changeColor(){
+    
+//     var colors = ['EE352E','00933C', 'B933AD']
+
+//     document.querySelector('p').style.color = 'red'
+    
+
+
+// }
+
+
+
+
+
