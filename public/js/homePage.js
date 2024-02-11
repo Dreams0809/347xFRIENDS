81419fb6ffff1fa1de6921da40729fd61f9cd514
Dreams0809 <expensivepain9@gmail.com>
2024-02-11 17:28:02 -0500

    first commit

---
+++ public/js/homePage.js
@@ -0,0 +1,21 @@
+const colors = ['EE352E', '00933C', 'B933AD'];
+
+const colorElements = document.querySelectorAll('.font').innerText;
+
+// colorElements.forEach(
+// 	(el) => 
+//         (el.style.color = '#' + colors[Math.floor(Math.random() * colors.length)])    
+// );
+
+function colorWheel(colorElements){
+    console.log(colorElements)
+    
+}
+
+
+
+
+
+
+
+
